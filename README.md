a0d3186aae2e95d6c5baa0c12d0d3e2547b9e1ab
GOTO_TAKUMA <takuma_goto@waku-2.com>
2018-11-20 02:14:11 +0900

    make base

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## 概要
+Golangのスタートアップ用dockerプロジェクト
+
 ## 前提
 [Docker](https://docs.docker.com/install/)をインストール  
 
---
+++ app/controller/app.go
@@ -0,0 +1,9 @@
+package controller
+
+import (
+	"net/http"
+)
+
+// Index is method to render Top page.
+func Index(rw http.ResponseWriter, request *http.Request) {
+}
